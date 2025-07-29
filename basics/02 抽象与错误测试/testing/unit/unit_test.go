fa3bd6937fb9abf2b2f5d5545bc05dca13b6777d
安如衫 <wust.durui@gmail.com>
2025-07-29 16:58:05 +0800

    docs: 增加单元测试注释

--- basics/02 抽象与错误测试/testing/unit/unit_test.go
+++ basics/02 抽象与错误测试/testing/unit/unit_test.go
@@ -11,7 +11,10 @@ func malSquare(i int) int {
 	return i*i + rand.Intn(3)
 }
 
+// 单元测试：对最小可测试单元进行检查和验证
+// 原则：自动化（至少不能通过打印输出来检查）、独立性（不依赖其他测试用例）、可重复（Mock 框架模拟第三方资源）
 func TestSquare(t *testing.T) {
+	// Arrange
 	// 表格测试/表驱动测试
 	cases := []struct {
 		input    int
@@ -22,9 +25,10 @@ func TestSquare(t *testing.T) {
 		{3, 9},
 	}
 	for _, c := range cases {
-		// 也可以使用 t.Fail/Error/Fatal
+		// Act
 		got := malSquare(c.input)
 		want := c.expected
-		assert.Equal(t, want, got)
+		// Assert
+		assert.Equal(t, want, got) // 也可以使用 t.Fail/Error/Fatal
 	}
 }
